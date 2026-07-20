3f3aebf3c4970284a28318deac81bdd42122410d
bksipes215-bit <bksipes215@mail.cfcc.edu>
2026-07-20 16:01:17 -0400

    Add PHP database connection

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$conn = new mysqli("localhost", "root", "mysql", "mysql");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else {
+  echo "Connection successful!";
+}
+
+$conn->close();
+?>
